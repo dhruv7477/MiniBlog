f2f1a880159a5cf3f2e6c5566f0fd5bc9c943f6f
dhruv7477 <dhruv7477@gmail.com>
2023-03-22 11:34:19 +0530

    Updated Front and Backend of all the pages!

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, HttpResponseRedirect
+from .forms import SignUpForm, LoginForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .models import Post
 
 # Home
 def home(request):
-    return render(request, 'blog/home.html')
+    posts = Post.objects.all()
+    return render(request, 'blog/home.html', {'posts':posts})
 
 # About
 def about(request):
@@ -14,16 +19,43 @@ def contact(request):
 
 # Dashboard
 def dashboard(request):
-    return render(request, 'blog/dashboard.html')
+    if request.user.is_authenticated:
+        posts = Post.objects.all()
+        return render(request, 'blog/dashboard.html', {'posts':posts})
+    else:
+        return HttpResponseRedirect('/LogIn/')
 
 # LogIn
 def LogIn(request):
-    return render(request, 'blog/login.html')
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = LoginForm(request=request, data=request.POST)
+            if form.is_valid():
+                uname = form.cleaned_data['username']
+                upass = form.cleaned_data['password']
+                user = authenticate(username=uname, password=upass)
+                if user is not None:
+                    login(request, user)
+                    messages.success(request, 'Logged in Successfully!')
+                    return HttpResponseRedirect('/dashboard/')
+        else:
+            form = LoginForm()
+        return render(request, 'blog/login.html', {'form':form})
+    else:
+        return HttpResponseRedirect('/dashboard/')
 
 # SignUp
 def SignUp(request):
-    return render(request, 'blog/signup.html')
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'Congratulations, you have become an Author!')
+            form.save()
+    else:
+        form = SignUpForm()
+    return render(request, 'blog/signup.html', {'form':form})
 
 # LogOut
 def user_logout(request):
+    logout(request)
     return HttpResponseRedirect('/')
